13e0d73ea02433513196098d98ec3cd4c3368cdd
Sviatoslav <sv_dubov@ukr.net>
2020-04-15 12:13:10 +0300

    Day 8. queryStringParams in API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 //import { moviesData } from "../moviesData";
 import MovieItem from "./MovieItem";
 import { API_URL, API_KEY_3 } from "../utils/api";
+import MovieTabs from "./MovieTabs";
 
 // UI = fn(state, props)
-
 // App = new React.Component()
 
 class App extends React.Component {
@@ -13,12 +13,13 @@ class App extends React.Component {
 
     this.state = {
       movies: [],
-      moviesWillWatch: []
+      moviesWillWatch: [],
+      sort_by: "popularity.desc"
     };
   }
 
   componentDidMount() {
-    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}`).then((response) => {
+    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`).then((response) => {
       return response.json()
     }).then((data) => {
       this.setState({
@@ -57,12 +58,25 @@ class App extends React.Component {
     });
   };
 
+  updateSortBy = value => {
+    this.setState({
+      sort_by: value
+    });
+  };
+
   render() {
     console.log("render", this);
     return (
       <div className="container">
         <div className="row mt-4">
           <div className="col-9">
+            <div className="row mb-4">
+              <div className="col-12">
+                <MovieTabs 
+                sort_by={this.state.sort_by}
+                updateSortBy={this.updateSortBy} />
+              </div>
+            </div>
             <div className="row">
               {this.state.movies.map(movie => {
                 return (
